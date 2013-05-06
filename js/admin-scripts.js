836ced9ca70125fad8c27bb04cca7c0a4b205adf
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-05-06 11:21:48 +0300

    Remove admin scripts. It's not needed in this plugin.

--- js/admin-scripts.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function ($) {
-	
-	// date picker
-	//var dateFormat = 'mm/dd/yy';
-	$('.edd_download_info_datepicker').datepicker({
-		dateFormat: datepicker_settings_vars.dateformat
-	});
-});
